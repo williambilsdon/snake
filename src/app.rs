76339536fbeab3ac2a7d272eb58151cec970b05d
William Bilsdon <williambilsdon@gmail.com>
2024-08-19 20:04:41 +0100

    switch to use component with ticker

--- src/app.rs
+++ src/app.rs
@@ -1,75 +1,50 @@
-use yew::prelude::*;
+use std::time::Duration;
 
-enum Direction {
-    North(u8, u8),
-    East(u8, u8),
-    South(u8, u8),
-    West(u8, u8),
+use futures::{Stream, StreamExt};
+use yew::{platform::time::interval, prelude::*};
+
+enum Message {
+    Tick,
 }
 
-struct Snake {
-    current_direction: Direction,
+struct Game {
+    tick: u8,
 }
 
-enum SnakePart {
-    Head,
-    Body,
+fn initialise_ticker() -> impl Stream<Item = ()> {
+    interval(Duration::new(1, 0))
 }
 
-enum State {
-    Snake(SnakePart),
-    Apple,
-    Nothing,
-}
+impl Component for Game {
+    type Message = Message;
 
-struct Square {
-    state: State,
-}
+    type Properties = ();
 
-impl Square {
-    pub fn new() -> Self {
-        Square {
-            state: State::Nothing,
+    fn create(ctx: &Context<Self>) -> Self {
+        let ticks = initialise_ticker();
+        ctx.link().send_stream(ticks.map(|_| Message::Tick));
+        Game { tick: 0 }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <p> {self.tick}</p>
         }
     }
-}
 
-struct Board {
-    squares: Squares,
-}
-
-type Squares = [[Square; 32]; 32];
-
-impl Board {
-    pub fn new() -> Self {
-        let squares: Squares = core::array::from_fn(|_| core::array::from_fn(|_| Square::new()));
-
-        Board { squares }
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        self.tick = self.tick + 1;
+        true
     }
 }
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let square_size = "30px";
-    let grid_style = format!(
-        "display: grid; grid-template-columns: repeat(32, {square_size}); \
-             grid-template-rows: repeat(32, {square_size});",
-    );
-    let board = Board::new();
-    let squares: Html = board.squares.iter().flat_map(|row| row.iter().map(|_| {
-        let color = format!("rgb({}, {}, {})", 166, 166, 166);
-
-        html! {
-            <div style={format!("background-color: {}; width: {square_size}; height: {square_size}; border-style: solid", color)}></div>
-        }
-    })).collect::<Html>();
-
     html! {
         <main>
-        <div style={grid_style}>
-            { squares }
-        </div>
-        // <div class="snake"> </div>
+            <div>
+                <Game/>
+            </div>
         </main>
     }
 }
